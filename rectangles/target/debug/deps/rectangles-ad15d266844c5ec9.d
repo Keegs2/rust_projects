73e02c389446e57b4c0d19325b197044c9a6244c
Keegan <KeGrabhorn@granitenet.com>
2025-01-24 09:56:02 -0500

    rust learning projects

---
+++ rectangles/target/debug/deps/rectangles-ad15d266844c5ec9.d
@@ -0,0 +1,5 @@
+/Users/keegan/rust_projects/rectangles/target/debug/deps/rectangles-ad15d266844c5ec9: src/main.rs
+
+/Users/keegan/rust_projects/rectangles/target/debug/deps/rectangles-ad15d266844c5ec9.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/Users/keegan/rust_projects/rectangles/target/debug/rectangles: /Users/keegan/rust_projects/rectangles/src/main.rs
